88c001b6c82c1c6159fddab5bb05a3d863a0d4ec
Masakazu Ishihata <masakazu.ishihata@gmail.com>
2014-12-01 13:25:51 +0900

    綺麗に書き直し

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 # Makefile
 TAR = ilda
 CC = gcc -O6 -Wall
-L = -lm -lirand -licsv -liutil
-OBJS = main.o ilda.o
+L = -lm -lirand -licsv -litime
+OBJS = main.o ilda_main.o ilda_vb.o ilda_cgs.o ilda_io.o
 
 all : $(TAR)
 $(TAR): $(OBJS)
@@ -13,5 +13,8 @@ clean:
 .c.o:	  $(CC) -c $<om
 
 
-main.o: main.h ilda.o
-ilda.o: ilda.h
+main.o: main.h ilda.h
+ilda_main.o: ilda_main.h
+ilda_vb.o: ilda_vb.h ilda_main.o
+ilda_cgs.o: ilda_cgs.h ilda_main.o
+ilda_io.o: ilda_io.h ilda_main.o
--- main.c
+++ main.c
@@ -8,21 +8,22 @@ int main(int argc, char *argv[])
   /* arguments */
   /*------------------------------------*/
   /* options */
-  char input[256]  = "input.twd";
-  char output[256] = "model";
-  char load[256]   = "";
-  int topic = 5;
+  char train[256]  = "input.twd";
+  char test[256]   = "";
+  char model[256] = "model";
+  int K = 5;
   int seed  = (int)time(NULL);
   int iter  = 10;
   double radius = 1e-5;
 
   /* optarg */
   int opt = 0, opt_idx = 0;
-  char *opts = "I:O:L:i:s:r:k:C";
+  char *opts = "I:O:T:i:s:r:k:C";
   struct option long_opts[] = {
     {"input",     1, NULL, 'I'},
-    {"output",    1, NULL, 'O'},
-    {"load",      1, NULL, 'L'},
+    {"train",     1, NULL, 'I'},
+    {"model",     1, NULL, 'O'},
+    {"test",      1, NULL, 'T'},
     {"seed",      1, NULL, 's'},
     {"iter",      1, NULL, 'i'},
     {"radius",    1, NULL, 'r'},
@@ -33,19 +34,19 @@ int main(int argc, char *argv[])
   /* parsing */
   while((opt = getopt_long(argc, argv, opts, long_opts, &opt_idx)) != EOF){
     switch(opt){
-    case 'I': sprintf(input, "%s", optarg); break;
-    case 'O': sprintf(output, "%s", optarg); break;
-    case 'L': sprintf(load, "%s", optarg); break;
+    case 'I': sprintf(train, "%s", optarg); break;
+    case 'O': sprintf(model, "%s", optarg); break;
+    case 'T': sprintf(test,  "%s", optarg); break;
     case 'i': iter   = atoi(optarg); break;
     case 's': seed   = atoi(optarg); break;
-    case 'k': topic  = atoi(optarg); break;
+    case 'k': K      = atoi(optarg); break;
     case 'r': radius = atof(optarg); break;
     case 'h':
     default:
       printf("Usage : ./ilda [options]\n");
-      printf("    --input,      -I [string] : input twd file name\n");
-      printf("    --output,     -O [string] : output file name\n");
-      printf("    --load,       -L [string] : load file name\n");
+      printf("    --input,      -I [string] : train file name\n");
+      printf("    --model,      -O [string] : model name\n");
+      printf("    --test,       -T [string] : test file name\n");
       printf("    --seed,       -s [int]    : random seed\n");
       printf("    --iter,       -i [int]    : # iteration\n");
       printf("    --radius,     -r [double] : convergence radius\n");
@@ -57,36 +58,41 @@ int main(int argc, char *argv[])
   /*------------------------------------*/
   /* main */
   /*------------------------------------*/
-  /* init */
   isrand((unsigned int)seed);
-  m = ilda_new(input, topic);
-  printf("#### setting ####\n");
-  printf("Input     : %s\n", input);
-  printf("Output    : %s\n", output);
-  if(strlen(load) > 0)
-    printf("Load      : %s.{al, bt, alp, btp}\n", load);
+
+  /* new for train */
+  m = ilda_new(train, K);
+  m->iter = iter;
+  m->radius = radius;
+
+  /* setting */
+  printf("#### Setting ####\n");
+  printf("Model     : %s\n", model);
+  printf("Train     : %s\n", train);
+  if(strlen(test) > 0)
+    printf("test      : %s\n", test);
   printf("seed      : %d\n", seed);
-  printf("# iter    : %d\n", iter);
-  printf("radius    : %e\n", radius);
+  printf("# iter    : %d\n", m->iter);
+  printf("radius    : %e\n", m->radius);
   printf("# docs    : %d\n", m->D);
   printf("# topics  : %d\n", m->K);
   printf("# words   : %d\n", m->V);
 
-  /* load */
-  if(strlen(load) > 0)
-    ilda_import(m, load);
-
-  /* learn */
-  printf("\n#### learning ####\n");
-  m->iter = iter;
-  m->radius = radius;
-  ilda_vb_main(m);
-
-  /* export */
-  ilda_export(m, output);
-
-  /* free */
+  /* train */
+  printf("\n#### Training ####\n");
+  ilda_vb_train(m);
+  ilda_export(m, model);
+  printf("%Le\n", ilda_vb_perplexity(m));
   ilda_free(m);
+
+  /* test */
+  if(strlen(test) > 0){
+    printf("\n#### Test ####\n");
+    m = ilda_new(test, K);
+    ilda_import(m, model);
+    printf("%Le\n", ilda_vb_perplexity(m));
+    ilda_free(m);
+  }
 
   return 0;
 }
